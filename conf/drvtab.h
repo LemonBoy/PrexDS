07ba4a142059cd59f2d8461064d150c4d47525c8
Cyril Lugan <cyril@lugan.info>
2010-02-02 15:36:00 -0500

    tangible configuration for DeSmuME

--- conf/drvtab.h
+++ conf/drvtab.h
@@ -3,8 +3,16 @@
  */
 extern struct driver cons_driver;
 extern struct driver wscons_driver;
+extern struct driver lcd_driver;
+extern struct driver null_driver;
+extern struct driver zero_driver;
+extern struct driver ramdisk_driver;
 
 struct driver *driver_table[] = {
 	&cons_driver,
 	&wscons_driver,
+	&lcd_driver,
+	&null_driver,
+	&zero_driver,
+	&ramdisk_driver,
 };
